cf56ed20dc657f97aa72f1d0051da5691c9405fc
HMAgiel <hasyimagiel@gmail.com>
2025-01-20 11:13:37 +0700

    Add files via upload

---
+++ ev.py
@@ -0,0 +1,24 @@
+from netCDF4 import Dataset
+from wrf import getvar
+
+# Specify the path to your WRF output file
+wrfout_file = "wrfout_d01_2024-01-01_00:00:00"
+
+# Specify the output text file path
+output_file = "variables_list.txt"
+
+# Open the WRF output file
+ncfile = Dataset(wrfout_file)
+
+# List all variables in the WRF output file
+variables = list(ncfile.variables.keys())
+
+# Save variables to a text file in a single line
+with open(output_file, "w") as f:
+    f.write(", ".join(variables))
+
+# Print a success message
+print(f"Variable names have been written to {output_file} in a single line.")
+
+# Close the file
+ncfile.close()
